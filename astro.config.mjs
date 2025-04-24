201ae0195a48444fe99624f1014bbed48320e2c8
jezpoz <jesfor94@gmail.com>
2025-04-24 13:18:09 +0200

    add correct vercel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,27 +5,38 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jezpoz.dev',
-	experimental: {
-		fonts: [{
-			provider: fontProviders.google(),
-			name: 'Workbench',
-			cssVariable: "--font-workbench",
-			fallbacks: ["ui-serif", "Georgia", "Cambria", 'Times New Roman', "Times", "serif"],
-		}, {
-			provider: fontProviders.google(),
-			name: 'Funnel Sans',
-			cssVariable: "--font-funnel-sans",
-			fallbacks: ["ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-		}]
+  site: 'https://jezpoz.dev',
+
+  experimental: {
+      fonts: [{
+          provider: fontProviders.google(),
+          name: 'Workbench',
+          cssVariable: "--font-workbench",
+          fallbacks: ["ui-serif", "Georgia", "Cambria", 'Times New Roman', "Times", "serif"],
+      }, {
+          provider: fontProviders.google(),
+          name: 'Funnel Sans',
+          cssVariable: "--font-funnel-sans",
+          fallbacks: ["ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      }]
 	},
-	integrations: [
-		mdx(),
-		sitemap(),
+
+  integrations: [
+      mdx(),
+      sitemap(),
 	],
-	vite: {
-		plugins: [tailwindcss()],
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		}
+	}),
 });
